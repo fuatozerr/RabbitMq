68c7589821f5012a38dfde2e42e14d438ddad347
Fuat <fuatozerr23@gmail.com>
2022-07-30 11:53:27 +0300

    add queue data

--- RabbitMq.Publisher/Program.cs
+++ RabbitMq.Publisher/Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client;
 using System;
 using System.IO;
+using System.Text;
 
 namespace RabbitMq.Publisher
 {
@@ -9,17 +10,29 @@ namespace RabbitMq.Publisher
     {
         static void Main(string[] args)
         {
-           var factory = new ConnectionFactory ();
+            var factory = new ConnectionFactory();
             var builder = new ConfigurationBuilder()
                     .AddJsonFile($"appsettings.json", true, true);
 
             var config = builder.Build();
             var connectionString = config["ConnectionStringRabbitMQ"];
-          //  var emailHost = config["Smtp:Host"];
+            //  var emailHost = config["Smtp:Host"];
             factory.Uri = new Uri(connectionString);
+            for (int i = 0; i < 10; i++)
+            {
+                
 
-            using var connection = factory.CreateConnection();
-            
+                using var connection = factory.CreateConnection();
+                string routeKey = "hello-queue";
+                var channel = connection.CreateModel();
+                channel.QueueDeclare(routeKey, true, false, false);
+                string message = "Hello rabbitmq :P";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(string.Empty, routeKey, null, messageBody);
+               
+            }
+            Console.WriteLine("mesaj gönderildi");
+            Console.ReadLine();
         }
     }
 }
