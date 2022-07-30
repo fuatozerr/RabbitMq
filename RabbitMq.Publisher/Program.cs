debfe2a77f12dc94922ac0ce4f1dbd452fb48f90
Fuat <fuatozerr23@gmail.com>
2022-07-30 11:32:21 +0300

    adding projects

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMq.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
