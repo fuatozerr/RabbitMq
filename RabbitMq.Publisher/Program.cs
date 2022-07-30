4e7a1e7444a48eba3c9f6cc0a41fe03952753ba6
Fuat <fuatozerr23@gmail.com>
2022-07-30 11:42:27 +0300

    adding connection json and connect to rabbgit mq

--- RabbitMq.Publisher/Program.cs
+++ RabbitMq.Publisher/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System;
+using System.IO;
 
 namespace RabbitMq.Publisher
 {
@@ -6,7 +9,17 @@ namespace RabbitMq.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var factory = new ConnectionFactory ();
+            var builder = new ConfigurationBuilder()
+                    .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+            var connectionString = config["ConnectionStringRabbitMQ"];
+          //  var emailHost = config["Smtp:Host"];
+            factory.Uri = new Uri(connectionString);
+
+            using var connection = factory.CreateConnection();
+            
         }
     }
 }
