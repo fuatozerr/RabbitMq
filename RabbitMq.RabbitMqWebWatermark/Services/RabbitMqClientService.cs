120192a5078401e7eb68ddc75a5c283804c77e72
Fuat <fuatozerr23@gmail.com>
2022-07-31 13:37:21 +0300

    adding rabbitmqservice

---
+++ RabbitMq.RabbitMqWebWatermark/Services/RabbitMqClientService.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+
+namespace RabbitMq.RabbitMqWebWatermark.Services
+{
+    public class RabbitMqClientService
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _channel;
+        public static string ExchangeName = "ImageDirectExchange";
+        public static string RoutingWatermark = "watermark-route-image";
+        public static string QueueName = "queue-watermark-image";
+    }
+}
