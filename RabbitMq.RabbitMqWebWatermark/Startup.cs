e810bcce2f3004f2994b9b90a2223b3b723f1d84
Fuat <fuatozerr23@gmail.com>
2022-08-13 14:24:00 +0300

    adding watermark and finished.

--- RabbitMq.RabbitMqWebWatermark/Startup.cs
+++ RabbitMq.RabbitMqWebWatermark/Startup.cs
@@ -28,7 +28,7 @@ namespace RabbitMq.RabbitMqWebWatermark
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")) });
+            services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")) ,DispatchConsumersAsync=true});
 
             services.AddSingleton<RabbitMqClientService>();
             services.AddSingleton<RabbitMQPublisher>();
