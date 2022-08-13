d1f5341abae29f676c3e39e544585ebdc0550a33
Fuat <fuatozerr23@gmail.com>
2022-08-13 14:16:16 +0300

    adding bg service

---
+++ RabbitMq.RabbitMqWebWatermark/BackGroundService/ImageWatermarkProcessBackgroundService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMq.RabbitMqWebWatermark.Services;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMq.RabbitMqWebWatermark.BackGroundService
+{
+    public class ImageWatermarkProcessBackgroundService : BackgroundService
+    {
+        private readonly RabbitMqClientService _rabbitMQClientService;
+        private readonly ILogger<ImageWatermarkProcessBackgroundService> _logger;
+        private IModel _channel;
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new System.NotImplementedException();
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
